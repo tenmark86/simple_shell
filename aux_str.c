8d31a83113196b6e078d95424cd6b02561d797a8
Ken Kamau <20206809+Ken-Kamau@users.noreply.github.com>
2022-05-18 21:44:24 +0300

    Update aux_str.c

--- aux_str.c
+++ aux_str.c
@@ -4,7 +4,7 @@
  * _strcat - concatenate two strings
  * @dest: char pointer the dest of the copied str
  * @src: const char pointer the source of str
- * Return: the 
+ * Return: the dest
  */
 char *_strcat(char *dest, const char *src)
 {
